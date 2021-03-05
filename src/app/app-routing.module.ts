fbf1ed77815c44878626e5f8a3c83874f1d70614
Carlos Gonzalez <carlosigonz@gmail.com>
2021-03-05 15:56:30 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListaComponent } from './components/lista/lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'lista', component: ListaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
