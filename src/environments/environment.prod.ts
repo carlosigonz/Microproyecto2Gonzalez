efd92bdb073c2f82ba723d46acc908b8f13027a7
Carlos Gonzalez <carlosigonz@gmail.com>
2021-03-06 07:58:31 -0400

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyA97YsKQrAmKD-EkXB9vBm8z2cRjpkcbVE",
+    authDomain: "microproyecto2gonzalez.firebaseapp.com",
+    projectId: "microproyecto2gonzalez",
+    storageBucket: "microproyecto2gonzalez.appspot.com",
+    messagingSenderId: "631891243031",
+    appId: "1:631891243031:web:e248054390c1fff8686c52",
+    measurementId: "G-RDXHJSRB3B"
+  }
 };
